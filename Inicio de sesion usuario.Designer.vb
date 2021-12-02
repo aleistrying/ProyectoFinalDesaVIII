c7f255215b4bfd1d1cdfc9bacd7830482a71429c
Alejandro P <alejandroparparcen@yahoo.com>
2021-12-02 00:26:09 -0500

    labelNmes

--- Inicio de sesion usuario.Designer.vb
+++ Inicio de sesion usuario.Designer.vb
@@ -22,74 +22,75 @@ Partial Class Inicio_de_sesion_usuario
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.imagenPlato = New System.Windows.Forms.PictureBox()
-        Me.Btn_platoAnterior = New System.Windows.Forms.Button()
-        Me.Btn_siguienteplato = New System.Windows.Forms.Button()
-        Me.BotonPrecio_Plato = New System.Windows.Forms.Button()
-        Me.labelnumPlato = New System.Windows.Forms.Label()
-        Me.labelNombrePlato = New System.Windows.Forms.Label()
+        Me.ImagenPlato = New System.Windows.Forms.PictureBox()
+        Me.BtnPlatoAnterior = New System.Windows.Forms.Button()
+        Me.BtnSiguientePlato = New System.Windows.Forms.Button()
+        Me.BtnDetallePlato = New System.Windows.Forms.Button()
+        Me.LblIdPlato = New System.Windows.Forms.Label()
+        Me.LblNombrePlato = New System.Windows.Forms.Label()
         Me.Btn_ir_al_carrito = New System.Windows.Forms.Button()
-        CType(Me.imagenPlato, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.ImagenPlato, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
-        'imagenPlato
+        'ImagenPlato
         '
-        Me.imagenPlato.Location = New System.Drawing.Point(309, 160)
-        Me.imagenPlato.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
-        Me.imagenPlato.Name = "imagenPlato"
-        Me.imagenPlato.Size = New System.Drawing.Size(284, 218)
-        Me.imagenPlato.TabIndex = 0
-        Me.imagenPlato.TabStop = False
+        Me.ImagenPlato.Location = New System.Drawing.Point(309, 160)
+        Me.ImagenPlato.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.ImagenPlato.Name = "ImagenPlato"
+        Me.ImagenPlato.Size = New System.Drawing.Size(284, 218)
+        Me.ImagenPlato.TabIndex = 0
+        Me.ImagenPlato.TabStop = False
         '
-        'Btn_platoAnterior
+        'BtnPlatoAnterior
         '
-        Me.Btn_platoAnterior.Location = New System.Drawing.Point(75, 220)
-        Me.Btn_platoAnterior.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
-        Me.Btn_platoAnterior.Name = "Btn_platoAnterior"
-        Me.Btn_platoAnterior.Size = New System.Drawing.Size(172, 77)
-        Me.Btn_platoAnterior.TabIndex = 1
-        Me.Btn_platoAnterior.Text = "Anterior"
-        Me.Btn_platoAnterior.UseVisualStyleBackColor = True
+        Me.BtnPlatoAnterior.Location = New System.Drawing.Point(75, 220)
+        Me.BtnPlatoAnterior.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.BtnPlatoAnterior.Name = "BtnPlatoAnterior"
+        Me.BtnPlatoAnterior.Size = New System.Drawing.Size(172, 77)
+        Me.BtnPlatoAnterior.TabIndex = 1
+        Me.BtnPlatoAnterior.Text = "Anterior"
+        Me.BtnPlatoAnterior.UseVisualStyleBackColor = True
         '
-        'Btn_siguienteplato
+        'BtnSiguientePlato
         '
-        Me.Btn_siguienteplato.Location = New System.Drawing.Point(652, 220)
-        Me.Btn_siguienteplato.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
-        Me.Btn_siguienteplato.Name = "Btn_siguienteplato"
-        Me.Btn_siguienteplato.Size = New System.Drawing.Size(172, 77)
-        Me.Btn_siguienteplato.TabIndex = 2
-        Me.Btn_siguienteplato.Text = "Siguiente"
-        Me.Btn_siguienteplato.UseVisualStyleBackColor = True
+        Me.BtnSiguientePlato.Location = New System.Drawing.Point(652, 220)
+        Me.BtnSiguientePlato.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.BtnSiguientePlato.Name = "BtnSiguientePlato"
+        Me.BtnSiguientePlato.Size = New System.Drawing.Size(172, 77)
+        Me.BtnSiguientePlato.TabIndex = 2
+        Me.BtnSiguientePlato.Text = "Siguiente"
+        Me.BtnSiguientePlato.UseVisualStyleBackColor = True
         '
-        'BotonPrecio_Plato
+        'BtnDetallePlato
         '
-        Me.BotonPrecio_Plato.Location = New System.Drawing.Point(309, 457)
-        Me.BotonPrecio_Plato.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
-        Me.BotonPrecio_Plato.Name = "BotonPrecio_Plato"
-        Me.BotonPrecio_Plato.Size = New System.Drawing.Size(284, 46)
-        Me.BotonPrecio_Plato.TabIndex = 3
-        Me.BotonPrecio_Plato.Text = "Button3"
-        Me.BotonPrecio_Plato.UseVisualStyleBackColor = True
+        Me.BtnDetallePlato.Location = New System.Drawing.Point(309, 457)
+        Me.BtnDetallePlato.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.BtnDetallePlato.Name = "BtnDetallePlato"
+        Me.BtnDetallePlato.Size = New System.Drawing.Size(284, 46)
+        Me.BtnDetallePlato.TabIndex = 3
+        Me.BtnDetallePlato.Text = "LblBtnPrecioPlato"
+        Me.BtnDetallePlato.UseVisualStyleBackColor = True
         '
-        'labelnumPlato
+        'LblIdPlato
         '
-        Me.labelnumPlato.AutoSize = True
-        Me.labelnumPlato.Location = New System.Drawing.Point(417, 383)
-        Me.labelnumPlato.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
-        Me.labelnumPlato.Name = "labelnumPlato"
-        Me.labelnumPlato.Size = New System.Drawing.Size(57, 20)
-        Me.labelnumPlato.TabIndex = 4
-        Me.labelnumPlato.Text = "Label1"
+        Me.LblIdPlato.AutoSize = True
+        Me.LblIdPlato.Location = New System.Drawing.Point(353, 383)
+        Me.LblIdPlato.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.LblIdPlato.Name = "LblIdPlato"
+        Me.LblIdPlato.Size = New System.Drawing.Size(187, 20)
+        Me.LblIdPlato.TabIndex = 4
+        Me.LblIdPlato.Text = "ID? no se si agregar esto"
+        Me.LblIdPlato.Visible = False
         '
-        'labelNombrePlato
+        'LblNombrePlato
         '
-        Me.labelNombrePlato.AutoSize = True
-        Me.labelNombrePlato.Location = New System.Drawing.Point(417, 432)
-        Me.labelNombrePlato.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
-        Me.labelNombrePlato.Name = "labelNombrePlato"
-        Me.labelNombrePlato.Size = New System.Drawing.Size(57, 20)
-        Me.labelNombrePlato.TabIndex = 5
-        Me.labelNombrePlato.Text = "Label1"
+        Me.LblNombrePlato.AutoSize = True
+        Me.LblNombrePlato.Location = New System.Drawing.Point(417, 432)
+        Me.LblNombrePlato.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.LblNombrePlato.Name = "LblNombrePlato"
+        Me.LblNombrePlato.Size = New System.Drawing.Size(57, 20)
+        Me.LblNombrePlato.TabIndex = 5
+        Me.LblNombrePlato.Text = "Label1"
         '
         'Btn_ir_al_carrito
         '
@@ -98,7 +99,7 @@ Partial Class Inicio_de_sesion_usuario
         Me.Btn_ir_al_carrito.Name = "Btn_ir_al_carrito"
         Me.Btn_ir_al_carrito.Size = New System.Drawing.Size(284, 103)
         Me.Btn_ir_al_carrito.TabIndex = 6
-        Me.Btn_ir_al_carrito.Text = "Ir al carrito"
+        Me.Btn_ir_al_carrito.Text = "Ir al carrito #"
         Me.Btn_ir_al_carrito.UseVisualStyleBackColor = True
         '
         'Inicio_de_sesion_usuario
@@ -107,26 +108,26 @@ Partial Class Inicio_de_sesion_usuario
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(892, 711)
         Me.Controls.Add(Me.Btn_ir_al_carrito)
-        Me.Controls.Add(Me.labelNombrePlato)
-        Me.Controls.Add(Me.labelnumPlato)
-        Me.Controls.Add(Me.BotonPrecio_Plato)
-        Me.Controls.Add(Me.Btn_siguienteplato)
-        Me.Controls.Add(Me.Btn_platoAnterior)
-        Me.Controls.Add(Me.imagenPlato)
+        Me.Controls.Add(Me.LblNombrePlato)
+        Me.Controls.Add(Me.LblIdPlato)
+        Me.Controls.Add(Me.BtnDetallePlato)
+        Me.Controls.Add(Me.BtnSiguientePlato)
+        Me.Controls.Add(Me.BtnPlatoAnterior)
+        Me.Controls.Add(Me.ImagenPlato)
         Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.Name = "Inicio_de_sesion_usuario"
         Me.Text = "Inicio_de_sesion_usuario"
-        CType(Me.imagenPlato, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.ImagenPlato, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
 
-    Friend WithEvents imagenPlato As PictureBox
-    Friend WithEvents Btn_platoAnterior As Button
-    Friend WithEvents Btn_siguienteplato As Button
-    Friend WithEvents BotonPrecio_Plato As Button
-    Friend WithEvents labelnumPlato As Label
-    Friend WithEvents labelNombrePlato As Label
+    Friend WithEvents ImagenPlato As PictureBox
+    Friend WithEvents BtnPlatoAnterior As Button
+    Friend WithEvents BtnSiguientePlato As Button
+    Friend WithEvents BtnDetallePlato As Button
+    Friend WithEvents LblIdPlato As Label
+    Friend WithEvents LblNombrePlato As Label
     Friend WithEvents Btn_ir_al_carrito As Button
 End Class
